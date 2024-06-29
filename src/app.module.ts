96d7712845420e7f87d20ecfd708a35e398ba0c4
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2024-06-29 21:38:19 +0700

    feat:contacts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 
 import { UserModule } from './user/user.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [CommonModule, UserModule],
+  imports: [CommonModule, UserModule, ContactModule],
   providers: [],
   exports: [],
 })
